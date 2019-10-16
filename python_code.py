80a7e83f3233ec2f449332971151b4c2a54439f9
Johns George <johnsg811@gmail.com>
2019-10-16 21:26:27 +0100

    Week1

--- python_code.py
+++ python_code.py
@@ -1,1 +1,7 @@
-print("Hello Github!")
+#Week 1 - This notebook will be primarly used for capstone project
+#Importing Pandas and Numpy
+
+import pandas as pd 
+import numpy as np 
+
+print("Hello Capstone Project Course!")
